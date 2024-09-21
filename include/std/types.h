7cfaf64eb4c92d3e9e1437efcc517713221be1fc
Dan McFarlane <dsmcfarl@gmail.com>
2024-09-21 10:42:26 -0500

    std: document types used in SYNOPSIS in kernel.s

---
+++ include/std/types.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// Integer types.
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long int64;
+typedef unsigned long uint64;
+
+// Boolean type.
+typedef uint8 bool;
+
+// Floating-point types.
+typedef float float32;
+typedef double float64;
+
+// Miscellaneous types.
+typedef uint8 byte;
