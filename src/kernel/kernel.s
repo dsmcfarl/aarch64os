47e49fd9fc11934560ca5220dd1854e87cb90530
Dan McFarlane <dsmcfarl@gmail.com>
2024-09-22 18:28:51 -0500

    properly load the device tree with QEMU and handle different address in kernel

--- src/kernel/kernel.s
+++ src/kernel/kernel.s
@@ -84,7 +84,8 @@ init_el3:
 	// device tree address defined in config.txt: device_tree_address=0x20000000
 	// must contain 0xD00DFEED (big-endian), 0xEDFE0DD0 (little-endian) in first 4 bytes
 	mov	x1,0x20000000	// address of device tree
-	ldrb	w0,[x1],1	// read first byte of device tree and increment address
+
+.init_el3_dtb:	ldrb	w0,[x1],1	// read first byte of device tree and increment address
 	cmp	x0,0xD0	// check if first byte is 0xD0
 	b.ne	.init_el3_bad_dtb	// if not, log
 	ldrb	w0,[x1],1	// read second byte of device tree and increment address
@@ -98,7 +99,14 @@ init_el3:
 	b.ne	.init_el3_bad_dtb	// if not, log
 	b	.init_el3_good_dtb	// if all bytes are correct, continue
 
+.init_el3_try_qemu:	// try QEMU address for dtb
+	mov	x1,0	// address of device tree for qemu
+	b	.init_el3_dtb	// repeat device tree check
+
+
 .init_el3_bad_dtb:	// log that we found an invalid device tree and sleep
+	cmp	x1,0	// check which address we were reading from
+	b.ne	.init_el3_try_qemu	// try QEMU address for dtb
 	adr	x0,bad_dtb_msg
 	mov	x1,bad_dtb_msg_size
 	bl	write_bytes_pri_uart
@@ -251,11 +259,11 @@ init_el3_msg:	.ascii	"init EL3\n"
 	.set	init_el3_msg_size,(. - init_el3_msg)
 
 	.balign	8
-bad_dtb_msg:	.ascii	"invalid device tree found at 0x2000_0000\n"
+bad_dtb_msg:	.ascii	"ERROR: no valid device tree found\n"
 	.set	bad_dtb_msg_size,(. - bad_dtb_msg)
 
 	.balign	8
-good_dtb_msg:	.ascii	"valid device tree found at 0x2000_0000\n"
+good_dtb_msg:	.ascii	"INFO: found valid device tree \n"
 	.set	good_dtb_msg_size,(. - good_dtb_msg)
 
 	.balign	8
